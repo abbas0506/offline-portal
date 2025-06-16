45cef6d937bb3d23aedb8d96a1255cfd68846b64
ushnaishfaq <ushnaashfaq@gmail.com>
2025-06-16 04:25:25 -0700

    ushan 00

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,6 @@
 <body class="bg-gray-100 text-center p-10">
     <h1 class="text-4xl font-bold text-blue-600">Hello Tailwind from Laravel</h1>
     <p class="text-gray-600 mt-4">Tailwind CSS is working perfectly without CLI 🎉</p>
+    <p class="text-gray-600 mt-4">TIt is from mam ushan</p>
 </body>
 </html>
